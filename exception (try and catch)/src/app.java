40c6488902a394187d517600f0f51980ccbcc759
AdrielCorreiaC <81880825+adrielcorreia@users.noreply.github.com>
2022-08-30 00:53:29 -0300

    Add files via upload

---
+++ exception (try and catch)/src/app.java
@@ -0,0 +1,11 @@
+
+public class app {
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println(0+'1');
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
